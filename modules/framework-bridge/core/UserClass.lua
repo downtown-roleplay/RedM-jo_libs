df28ef5446337593d9a88fcd2f24d6efc75c0f03
sorambr <caecronos@hotmail.com>
2025-10-07 14:27:30 -0300

    Retorna identifier em jo.framework.UserClass:getIdentifiers

--- modules/framework-bridge/core/UserClass.lua
+++ modules/framework-bridge/core/UserClass.lua
@@ -73,6 +73,7 @@ function jo.framework.UserClass:getIdentifiers()
     return {
         id = tonumber(user?.id),
         charid = tonumber(character?.id),
+        identifier = tonumber(character?.identifier)
     }
 end
 
